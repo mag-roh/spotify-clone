24fdb349e2e48cbfe14554222156ce4fa246fe0b
Rohit <rt6magnetic.1999@gmail.com>
2023-10-17 20:15:40 +0530

    spotify-app

--- src/Reducer.js
+++ src/Reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false,
     item: null,
     //remove the token after everything development is finished, otherwise login process will not work properly
-    //token: 'BQBtOq2LygDrxKp9t_1jOVzWifJrgx-Xpzx8yDeKbt8vX5UmhbQ5v3M2yMS8_A8rFfQxA6htWvmNlFMw8O54iV9kmixUOzag0DfqqPpP3dA5qr0JXL1WTWjY7tMqYwcV0J7PoBKNgNInHqfiX5D4H8P8OpntfTwTE0W99uqt-GIHipm1dsl0A7_Hhc6C1wAjTrc',//setting default value of token as null
+    token:null //setting default value of token as null
 }
 //defines the current state and the action to be performed to change the current state
 const reducer = (state, action) => {
